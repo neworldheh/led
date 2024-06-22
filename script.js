500095c5435c3c15fb561e336ff824f097458be5
Staś <83009119+neworldheh@users.noreply.github.com>
2024-06-22 19:24:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -42,4 +42,8 @@ document.addEventListener('DOMContentLoaded', function() {
     line.addEventListener('mouseout', function() {
         profileDiv.style.marginTop = '60px';
     });
+    const icon = document.querySelector('.icon');
+    icon.addEventListener('click', function(){
+        
+    )};
 });
